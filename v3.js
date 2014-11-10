7237e7e596770f899d6201acabdd1c9ff625be2e
Joe Hildebrand <joe-github@cursive.net>
2014-11-09 20:30:06 -1000

    ol numbering types, artwork alignment

--- v3.js
+++ v3.js
@@ -1,3 +1,5 @@
+var romanize = require('romanize');
+
 function atv(e, a) {
   if (e == null) {return null;}
   var atr = e.attr(a);
@@ -48,3 +50,43 @@ var categories = {
 exports.status = function(e) {
   return categories[atv(e, 'category')];
 };
+
+function to26(n) {
+  if (n==0) {
+    return [0];
+  }
+  var rem = n;
+  var digs = [];
+  while (rem > 0) {
+    // for the first 26, we've already subtracted 1.
+    // For other digits, subtract 1 so that we can add to 'a' later
+    digs.unshift((rem % 26) - (digs.length>0? 1 : 0));
+    rem = Math.floor(rem/26)
+  }
+  return digs;
+}
+
+exports.olStyledNum = function(style, num) {
+  return style.replace(/%([cCdiIt%])/g, function(match, s) {
+    switch (s) {
+      case '%':
+        return '%';
+      case 't':
+        return to26(num-1).toString();
+      case 'c':
+        return to26(num-1).map(function (n) {
+          return String.fromCharCode('a'.charCodeAt(0)+n)
+        }).join('');
+      case 'C':
+        return to26(num-1).map(function (n) {
+          return String.fromCharCode('A'.charCodeAt(0)+n);
+        }).join('');
+      case 'd':
+        return "" + num;
+      case 'i':
+        return romanize(num).toLowerCase();
+      case 'I':
+        return romanize(num);
+    }
+  })
+}
