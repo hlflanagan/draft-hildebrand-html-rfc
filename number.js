092e01d35697b3da11d6a675827876eff892a430
Joe Hildebrand <joe-github@cursive.net>
2015-04-15 15:57:37 -0600

    fix a ton of bugs

--- number.js
+++ number.js
@@ -1,3 +1,9 @@
+var fs = require('fs');
+var dataUri = require('strong-data-uri');
+var url = require('url');
+var urllibsync = require('urllib-sync');
+var xml = require('libxmljs');
+
 exports.section = function(e) {
   var all = e.find('ancestor-or-self::section');
   return all.map(function(a){
@@ -92,3 +98,80 @@ exports.normalize = function(t) {
   t = t.replace(/\.\s*\n\s+/gm, '.  ');
   return t.replace(/\s*\n\s+/gm, ' ');
 };
+
+att = function(e, a) {
+  var attr = e.attr(a);
+  if (!attr) { return null; }
+  return attr.value();
+}
+
+exports.loadSrc = function(e) {
+  var src = att(e, 'src');
+  if (!src) { return; }
+  var typ = att(e, 'type');
+  if (!typ) {
+    process.stderr.write("'type' attribute suggested for src: " + src + "\n");
+  }
+
+  var types = ['ascii-art', 'call-flow', 'hex-dump', 'svg'];
+
+  // TODO: can base64 generate ".."?
+  if (src.match(/(^\/)|(\.\.)/)) {
+    process.stderr.write('Invalid src URL: ' + src + '\n');
+    return;
+  }
+  var cwd = process.cwd() + '/';
+  var u = url.resolve('file://' + cwd, src);
+  var up = url.parse(u, false, true);
+  if (!up) {
+    return;
+  }
+
+  var todata = false;
+  if (types.indexOf(typ) === -1) {
+    // For unknown types, turn the data into a data: if it's not already
+    if (u.protocol == 'data:') {
+      return;
+    }
+    todata = true;
+  }
+
+  var data = null;
+  var media = 'text/plain';
+
+  if (up.protocol === 'file:') {
+    // check to make sure we're in cwd or a subdir
+    if (up.pathname.slice(0,cwd.length) !== cwd) {
+      process.stderr.write('Invalid path: ' + up.pathname + '\n');
+      return;
+    }
+    data = fs.readFileSync(up.pathname);
+  } else if (up.protocol === 'data:') {
+    data = dataUri.decode(u);
+  } else if ((up.protocol === 'http:') || (up.protocol === 'http:')) {
+    // jade is strictly snychronous
+    var res = urllibsync.request(u);
+    if (res.status !== 200) {
+      process.stderr.write('HTTP error (' + res.statusCode + '): ' + u + '\n');
+      return;
+    }
+    data = res.data;
+    media = res.headers['content-type'];
+  } else {
+    process.stderr.write('Unknown URI scheme: ' + u + '\n');
+    return;
+  }
+
+  if (todata) {
+    var u = dataUri.encode(data, media);
+    e.attr({'src': u});
+  } else {
+    if (typ === 'svg') {
+      var svg = xml.parseXmlString(data);
+      e.addChild(svg.root());
+    } else {
+      e.cdata(data.toString('utf8'));
+    }
+    e.attr('src').remove();
+  }
+}
